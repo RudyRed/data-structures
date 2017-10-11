1d16a852ef14dc032cd18eca21e813bb8a74358a
RudyRed <willgross94@gmail.com>
2017-10-11 12:12:17 -0700

    Completed Functional portion of sprint one

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,30 @@
 var Queue = function() {
   var someInstance = {};
-
+  var count = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[JSON.stringify(count)] = value;
+    count++
   };
 
   someInstance.dequeue = function() {
+    var output = storage['0'];
+    if (count > 0) {
+      for (var i = 1; i < count; i++) {
+        storage[JSON.stringify(i - 1)] = storage[i]
+      }
+      count--
+    }
+    delete storage[JSON.stringify(count)];
+    return output;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,27 @@
 var Stack = function() {
   var someInstance = {};
-
+  var count = 0;
+  //var lastIn = undefined
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[JSON.stringify(count)] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+  var output = storage[JSON.stringify(count - 1)];
+  delete storage[JSON.stringify(count - 1)];
+  if (count > 0) {
+      count--;
+    }
+    return output;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
